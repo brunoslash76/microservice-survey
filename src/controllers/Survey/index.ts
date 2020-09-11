bb2b22e9931fb616a5f530dc02c0b3cb85b65519
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-09-11 13:34:38 -0300

    Refactor: Remove console.log()

--- src/controllers/Survey/index.ts
+++ src/controllers/Survey/index.ts
@@ -3,11 +3,7 @@ import { TData } from '../../types'
 import { saveAnswers } from '../../services/firebase'
 
 export const save = async (req: Request, res: Response) => {
-
-    console.log(req)
     const body: TData = req.body
-    console.log(body)
     const response = await saveAnswers(body)
-
     res.status(200).send({ success: true, userInfo: response })
 }
